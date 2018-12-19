4e35564395118b6d12fb0e5e98b87f4ed4818b7d
René van Gool <r.vangool@live.nl>
2018-12-19 11:12:04 +0100

    contact routes

--- routes/ContactRoutes.js
+++ routes/ContactRoutes.js
@@ -2,7 +2,7 @@ const contact_controller = require('../controllers/ContactController');
 const express = require('express');
 const router = express.Router();
 
-// router.post('/concerts', contact_controller.create)
+router.post('/contacts', contact_controller.create)
 // router.put('/concerts', contact_controller.edit)
 // router.delete('/concerts', contact_controller.delete)
 router.get('/contacts', contact_controller.get)
