ca40a9b7a40049c703bed9f8278fed5ee1a8cbc9
Reneetjuh21 <r.vangool@live.nl>
2018-12-20 12:56:00 +0100

    Update EmployeeRoutes.js

--- routes/EmployeeRoutes.js
+++ routes/EmployeeRoutes.js
@@ -5,7 +5,7 @@ const router = express.Router();
 router.post('/employees', employee_controller.create)
 // router.put('/concerts', employee_controller.edit)
 // router.delete('/concerts', employee_controller.delete)
-router.get('/employees', employee_controller.get)
+router.get('/employees', employee_controller.getByLabelId)
 router.get('/employees/:id', employee_controller.getById)
 
 module.exports = router;
