97dcd9a11b0a982a58867904bfe9178eb522b90b
Reneetjuh21 <r.vangool@live.nl>
2019-01-10 09:41:20 +0100

    invoice

--- routes/InvoiceRoutes.js
+++ routes/InvoiceRoutes.js
@@ -3,8 +3,8 @@ const express = require('express');
 const router = express.Router();
 
 router.post('/invoices', invoice_controller.create)
-// router.put('/concerts', invoice_controller.edit)
-// router.delete('/concerts', invoice_controller.delete)
+router.put('/invoices', invoice_controller.edit)
+router.delete('/invoices/:id', invoice_controller.delete)
 router.get('/invoices', invoice_controller.get)
 router.get('/invoices/:id', invoice_controller.getById)
 
